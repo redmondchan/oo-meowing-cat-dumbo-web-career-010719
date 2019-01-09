e118cb94d57aef1de22243577ab454c241d55bb4
Redmond Chan <redmondchan92@gmail.com>
2019-01-08 22:34:19 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,21 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+# def name
+#   @name
+# end
+#
+# def name=(new_name)
+#   @name = new_name
+# end
+
+attr_accessor :name
+
+def meow
+  @meow
+end
+
+def meow
+  puts "meow!"
+end
+
+end
